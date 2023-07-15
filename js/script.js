8b81fd10a80ef5e7f6cb83e420eb1227826365f3
[Razorsujith] <[sujith19961996@gmail.com]>
2023-07-15 16:34:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+const boxs=document.querySelectorAll('.box');
+const statusRazor=document.querySelector('#status');
+const btnRestart=document.querySelector('#restart');
+let x="<img src='../images/o.png'>";
+let o="<img src='../images/delete.png'>";
+
+
+const win=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+let options=["","","","","","","","",""];
+let currentplayer=x;
+let player="Razor";
+let running=false;
+init();
+
+
+function init(){
+boxs.forEach(box=>box.addEventListener('click',boxClick));
+btnRestart.addEventListener('click',restartGame);
+statusRazor.textContent=`${player} Your Turn`;
+running=true;
+}
+
+function boxClick(){
+    const index=this.dataset.index;
+    if(options[index]!= "" || !running){
+    return;
+    }
+    updateBox(this,index);
+    checkWinner();
+}
+
+function updateBox(box,index){
+    options[index]= player;
+    box.innerHTML = currentplayer;
+}
+
+function checkWinner(){
+    let isWon=false;
+    for(let i=0;i<win.length;i++){
+     const condition=win[i];   //[0,1,2]
+     const box1=options[condition[0]]; //x
+     const box2=options[condition[1]]; //''
+     const box3=options[condition[2]]; //''
+     if(box1=="" || box2=="" || box3==""){
+        continue;
+     }
+     if(box1==box2 && box2==box3){
+        isWon=true;
+        boxs[condition[0]].classList.add('win');
+        boxs[condition[1]].classList.add('win');
+        boxs[condition[2]].classList.add('win');
+     }
+    }
+    if(isWon){
+        statusRazor.textContent=`${player} Won..`;
+        running=false;
+
+    }else if(!options.includes("")){
+        statusRazor.textContent=`Game Draw..!`;
+        running=false;
+    }else{
+        changePlayer();
+    }
+
+}
+
+function changePlayer(){
+    player=(player=='Razor') ? "o" : "Razor";
+    currentplayer=(currentplayer==x) ? o : x;
+    statusRazor.textContent=`${player} Your Turn`;
+
+}
+
+function restartGame(){
+
+    options=["","","","","","","","",""];
+    currentplayer=x;
+    player="Razor";
+    running=true;
+    statusRazor.textContent=`${player} Your Turn`;
+
+
+    boxs.forEach(box=>{
+        box.innerHTML="";
+        box.classList.remove('win');
+    });
+}
